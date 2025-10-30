0abf3bd5f90b70ec1f838314e0cd4da9f0cf9de9
Mad-Mechanics-10698 <10698wms@gmail.com>
2025-10-30 16:27:29 -0400

    Create HelloWorld.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class HelloWorld extends OpMode {
+
+
+    @Override
+    public void init() {
+        telemetry.addData( "Hello","World");
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
